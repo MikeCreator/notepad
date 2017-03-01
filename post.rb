f0eb7181d70349d563ca4dd867d1df6f63282c44
Михаил Кривонос < >
2017-03-01 23:27:11 +0200

    post class + ancestors

--- post.rb
+++ post.rb
@@ -1,8 +1,17 @@
 class Post
 
-  deg initialize
-  @created_at = Time.now
-  @text = nill
+  def self.post_types
+    [Memo, Link, Task]
+  end
+
+  def self.create(type_index)
+    return post_types[type_index].new
+  end
+
+  def initialize
+    @created_at = Time.now
+    @text = nil
+  end
 
   def read_from_console
 
@@ -25,7 +34,8 @@ class Post
 
   def file_path
     current_path = File.dirname(__FILE__)
-    file_name = @created_at.strftime("#{self.class.name}_%Y.%m.%d_%H:%M:%S.txt")
-    return current_path + '/' + file.name
+    file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
+    return current_path + "/" + file_name
   end
+
 end
